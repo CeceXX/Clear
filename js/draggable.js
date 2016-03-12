2084cf9d89d50f91b67d400c687c6e6b3b935959
alexandru-bagi <alexandru.bagi@gmail.com>
2016-03-12 08:06:54 +0200

    Working drag nd drop ( it's a start, still needs work )

--- js/draggable.js
+++ js/draggable.js
@@ -1,5 +1,11 @@
-$( init );
- 
-function init() {
-  $('li').draggable();
-}
+dragula([document.getElementById("elements"), document.getElementById("sandbox")], {
+  copy: function (el, source) {
+    return source === document.getElementById("elements")
+  },
+  accepts: function (el, target) {
+    return target !== document.getElementById("elements")
+  }
+});
+dragula([document.getElementById('sandbox')], {
+  removeOnSpill: true
+});

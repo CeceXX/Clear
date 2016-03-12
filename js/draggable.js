5bb8b36b42a67895620c23c5eee6d9ba8ae851f5
CeceXX <hi@cesare.io>
2016-03-13 00:17:57 +0100

    minor ui changes

--- js/draggable.js
+++ js/draggable.js
@@ -10,3 +10,15 @@ dragula([document.getElementById('sandbox')], {
   removeOnSpill: true
 });
 
+// Hide thing
+
+var formBuilder = document.getElementById("formBuilder");
+
+window.onload = function() {
+	formBuilder.style.visibility = "hidden";
+}
+
+function showFormBuilder() {
+	console.log("show!!");
+	formBuilder.style.visibility = "visible";
+}
